fff2625781e85179db05f1423cf5d599a80bb5a6
Ricky Kuang <rkuang26@gmail.com>
2017-03-15 17:33:03 -0700

    lol what

--- parallel_code/bag.h
+++ parallel_code/bag.h
@@ -16,4 +16,48 @@ class Bag {
   Pennant** backbone;
 };
 
+class Bag_reducer {
+  public:
+    struct Monoid: cilk::monoid_base<Bag > {
+      static void reduce(Bag *left, Bag *right){
+        left->merge(right);
+      }
+    };
+
+    Bag_reducer() : imp_() {}
+
+    int get_backbone_size() {
+      return imp_.view().backbone_size;
+    }
+
+    Pennant* get_backbone(int i) {
+      return imp_.view().backbone[i];
+    }
+
+    void set_backbone(int i, Pennant* tree) {
+      imp_.view().backbone[i] = tree;
+    }
+
+    void bag_insert(int item) {
+      imp_.view().bag_insert(item);
+    }
+
+    void bag_union(Bag_reducer* that) {
+      this->imp_.view().bag_union(&that->imp_.view());
+    }
+
+    Bag* bag_split() {
+      return imp_.view().bag_split();
+    }
+
+    bool is_empty() const {
+      return imp_.view().is_empty();
+    }
+
+  private:
+    cilk::reducer<Monoid> imp_;
+
+  };
+}
+
 #endif

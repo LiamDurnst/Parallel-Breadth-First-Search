1e318bc9862f465b18465819ec965597e3cccd96
Josue <josuemy@gmail.com>
2017-03-17 17:08:37 -0700

    checked if bag index is not null

--- parallel_code/pbfs.cpp
+++ parallel_code/pbfs.cpp
@@ -132,7 +132,8 @@ void process_layer(graph* G, Bag* &in_bag, Bag_reducer* &out_bag,int thislevel, 
   if (in_bag->n_vertices() < 128) {
     cilk_for(int i = 0; i < in_bag->n_vertices(); i++) {
       cout << "before walk_bag ITER: " << i << endl;
-      walk_bag(G, in_bag->backbone[i]->root, out_bag, thislevel, level, parent);
+      if(in_bag->backbone[i]!=NULL)
+        walk_bag(G, in_bag->backbone[i]->root, out_bag, thislevel, level, parent);
       cout << "after walk_bag ITER: " << i << endl;
     }
     return;

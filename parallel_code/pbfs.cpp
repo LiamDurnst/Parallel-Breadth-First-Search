45f9a12cf72ca6da62833812066fbf41a535d04d
Ricky Kuang <whatever@example.com>
2017-03-17 18:37:05 -0700

    sequential

--- parallel_code/pbfs.cpp
+++ parallel_code/pbfs.cpp
@@ -101,7 +101,7 @@ void walk_bag(graph* G, Node* root, Bag_reducer* &out_bag, int thislevel, int* &
   int current_node = root->vertex;
   int end = G->firstnbr[current_node + 1];
   // cout << "Current node: " << current_node << endl;
-  cilk_for (int u = G->firstnbr[current_node]; u < end; u++) { // cilk_for
+  for (int u = G->firstnbr[current_node]; u < end; u++) { // cilk_for
     int current_neighbor = G->nbr[u];
     // cout<< "current_neighbor: " << current_neighbor << endl;
     if (level[current_neighbor] == -1) {
